9d021887ca6f9adae938128af81a4f920d5cca3f
Salman Zafar <salman.zafarurdin@publicisgroupe.com>
2023-05-25 15:26:17 +0400

    readin repo license information and displaying

--- src/RepoInfo.js
+++ src/RepoInfo.js
@@ -1,4 +1,24 @@
 export default function RepoInfo({repo}){
+    let license;
+
+    switch (repo.licenseInfo?.spdxId)
+    {
+        case undefined:
+            license = (<span className="px-1 py-0 ms-1 d-inline-block btn btn-sm btn-danger" style={{fontSize: ".6em"}}>
+                    No License
+                </span>)
+            break;
+        case "NOASSERTION":
+            license = (<span className="px-1 py-0 ms-1 d-inline-block btn btn-sm btn-warning" style={{fontSize: ".6em"}}>
+                {repo.licenseInfo?.spdxId}
+                </span>)
+            break;
+        default:
+            license = (<span className="px-1 py-0 ms-1 d-inline-block btn btn-sm btn-success" style={{fontSize: ".6em"}}>
+                {repo.licenseInfo?.spdxId}
+                </span>)
+            break;
+    }
     return <li className="list-group-item" >
         <div className="d-flex justify-content-between align-items-center">
             <div className="d-flex flex-column">
@@ -7,14 +27,13 @@ export default function RepoInfo({repo}){
                 </a>
                 <p className="small">{repo.description}</p>
             </div>
-            <span className={
-                "px-1 py-0 ms-1 d-inline-block btn btn-sm " +
-                (
-                    repo.viewerSubscription === "SUBSCRIBED" ? "btn btn-success" : "btn btn-outline-secondary"
-                )
-            }>
-                {repo.viewerSubscription}
-            </span>
+            <div className="text-nowrap ms-3">
+                {license}
+                <span className={"px-1 py-0 ms-1 d-inline-block btn btn-sm " +                    (repo.viewerSubscription === "SUBSCRIBED" ? "btn btn-success" : "btn btn-outline-secondary")}>
+                       {repo.viewerSubscription}
+                   </span>
+            </div>
+
         </div>
     </li>
 
